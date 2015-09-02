df69d7d21963edffa867869808c78b35b23e9e71
L2-D2 <omnilemm@gmail.com>
2015-09-01 20:05:09 -0700

    adds user password, add bcrypt to gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902024737) do
+ActiveRecord::Schema.define(version: 20150902030329) do
 
   create_table "grids", force: true do |t|
     t.integer  "user_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150902024737) do
     t.text     "favs"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
