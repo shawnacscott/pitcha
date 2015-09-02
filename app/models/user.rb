a76846c2066a6b8a01e052984e699b19e93e1280
L2-D2 <omnilemm@gmail.com>
2015-09-01 23:45:21 -0700

    moves index back to welcome#index

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :favs, dependent: :destroy
   has_many :grids, dependent: :destroy
+
+  def feed
+    Grid.where("user_id = ?", id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root    'grids#all'
+  root    'welcome#index'
   get     'favs'    =>  'users#show'
   get     'signup'  =>  'users#new'
   get     'login'   =>  'sessions#new'
